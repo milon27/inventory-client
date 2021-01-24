0302dddf5c1c03cb1c0db8cb49c89cabe17dd7d6
milon27 <mdjahidulislammilon@gmail.com>
2021-01-25 02:55:36 +0600

    successfull firebase next app deploy

---
+++ server.js
@@ -0,0 +1,18 @@
+const { https } = require('firebase-functions');
+const { default: next } = require('next');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+const server = next({
+    dev: isDev,
+    //location of .next generated after running -> yarn build
+    conf: { distDir: '.next' },
+});
+
+const nextjsHandle = server.getRequestHandler();
+exports.inventoryNextFun = https.onRequest((req, res) => {
+    return server.prepare()
+        .then(() => {
+            return nextjsHandle(req, res)
+        });
+});
